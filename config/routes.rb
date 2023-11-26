9b43549adeed8309fed14000184b62d4097d34be
mochimochi <hybridkazuhiko@yahoo.co.jp>
2023-11-26 21:31:26 +0900

    デプロイ(修正版)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "items#index"
+  #resources :
+  
+  #devise_for :users
+  #root to: "prototypes#index"
+  #resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
+    #resources :comments, only: :create 
 end
