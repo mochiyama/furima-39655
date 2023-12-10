30d90f19e14a035e740f169c4893311cbbd6ceb7
mochimochi <hybridkazuhiko@yahoo.co.jp>
2023-12-10 15:28:11 +0900

    usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,             presence: true
+  validates :email,   	           presence: true
+  validates :password, 	           presence: true
+  validates :last_name_kanji,      presence: true
+  validates :first_name_kanji,     presence: true
+  validates :last_name_kana,       presence: true
+  validates :first_name_kana,      presence: true
+  validates :birth_day,            presence: true
+       
+  has_many :items
+  has_many :purchasing_infos
+
 end
