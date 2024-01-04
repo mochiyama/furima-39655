0f5a735972588c675977126bc8ed910b6efc266e
mochimochi <hybridkazuhiko@yahoo.co.jp>
2024-01-04 21:14:16 +0900

    出品機能（指摘修正1）

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,27 +8,25 @@ class Item < ApplicationRecord
     belongs_to :ship_from
     belongs_to :shipping_date
 
-  #空の投稿を保存できないようにする
-  #validates :title, :text, presence: true #不要
-
-  #ジャンルの選択が「---」の時は保存できないようにする
-
-
   #has_one :purchasing_info
   has_one_attached :image
   
-  validates :image,              presence: true #不要？でコメントアウトした
+  validates :image,              presence: true 
 
   validates :item_name,          presence: true
   validates :description,        presence: true
 
-  validates :user,               presence: true
-  validates :category_id,        presence: true, numericality: { other_than: 1, message: "can't be blank"}
-  validates :condition_id,       presence: true, numericality: { other_than: 1, message: "can't be blank"}
-  validates :shipping_fee_id,    presence: true, numericality: { other_than: 1, message: "can't be blank"}
-  validates :ship_from_id,       presence: true, numericality: { other_than: 1, message: "can't be blank"}
-  validates :shipping_date_id,   presence: true, numericality: { other_than: 1, message: "can't be blank"}
 
-  validates :price,              presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "must be greater than or equal to 300"}
+  validates :category_id,         numericality: { other_than: 1, message: "can't be blank"}
+  validates :condition_id,        numericality: { other_than: 1, message: "can't be blank"}
+  validates :shipping_fee_id,     numericality: { other_than: 1, message: "can't be blank"}
+  validates :ship_from_id,        numericality: { other_than: 1, message: "can't be blank"}
+  validates :shipping_date_id,    numericality: { other_than: 1, message: "can't be blank"}
+
+           
+  validates :price,               presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "must be greater than 299 or less than 10000000"}, format: { with: /\A[0-9]+\z/, message: "must be half-width" }
+  #validates :price,               format: { with: /\A[0-9]+\z/ }
+
+
 
 end
--- db/migrate/20231224010848_create_items.rb
+++ db/migrate/20231224010848_create_items.rb
@@ -7,7 +7,7 @@ class CreateItems < ActiveRecord::Migration[7.0]
       t.text        :description,        null: false
       t.integer     :price,              null: false
       t.references  :user,               null: false, foreign_key:true
-      t.integar     :category_id,        null: false
+      t.integer     :category_id,        null: false
       t.integer     :condition_id,       null: false
       t.integer     :shipping_fee_id,    null: false
       t.integer     :ship_from_id,       null: false
