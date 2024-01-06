63a86064596cc64594f084f79e47c0f91a0f1217
mochimochi <hybridkazuhiko@yahoo.co.jp>
2024-01-06 21:40:04 +0900

    編集機能実装完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,13 +25,19 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
   end 
 
+
+
   def update
+   @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to root_path
+      redirect_to item_path
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end 
 
