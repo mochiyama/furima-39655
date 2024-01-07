e6420f90f6f280185f3077479cf8b3293edf0909
mochimochi <hybridkazuhiko@yahoo.co.jp>
2024-01-07 16:13:04 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index , :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update] #, :destroy
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -37,7 +37,6 @@ class ItemsController < ApplicationController
   end 
 
   #def destroy
-    #item = Item.find(params[:id])
     #item.destroy
   #end
 
