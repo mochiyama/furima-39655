26809d90b7db7abad47266ec7db233310573425e
mochimochi <hybridkazuhiko@yahoo.co.jp>
2024-01-07 17:19:20 +0900

    Revert "削除機能実装"
    This reverts commit 4f4a9608054bad147ec9b3c3025316202b99d69d.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index , :show]
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -36,11 +36,10 @@ class ItemsController < ApplicationController
     end
   end 
 
-  def destroy
-    if @item.destroy
-     redirect_to root_path
-    end
-  end
+  #def destroy
+    #item = Item.find(params[:id])
+    #item.destroy
+  #end
 
   private
   def item_params
