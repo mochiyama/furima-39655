9f37199414035bb7ea7b703ec418c3866d77c80c
mochimochi <hybridkazuhiko@yahoo.co.jp>
2024-01-15 12:40:30 +0900

    販売手数料計算式修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -7,7 +7,7 @@ const price = () => {
      addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
      const addPofitDom = document.getElementById("profit");
    
-     addPofitDom.innerHTML = Math.floor(priceInput.value - Math.round(priceInput.value * 0.1 ))
+     addPofitDom.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1 ))
  })
 };
 window.addEventListener("turbo:load", price);
